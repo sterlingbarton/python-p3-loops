bb4ed9829083aec8f0bbace88a097a3362f90e95
Sterling <sterlingbarton81@gmail.com>
2023-05-03 13:06:27 -0600

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i in range(10, 0, -1):
+        print(i)
+        i -= 1
+
+    print('Happy New Year!')
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_numbers = [num * num for num in int_list]
+    return square_numbers
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
